38da3f568811473b8ec3ceb319bed42420bd124a
Chinmay Parab <43921132+chinmayparab@users.noreply.github.com>
2020-06-25 17:29:12 +0530

    User fetch and Loader wait

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,40 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
-class Users extends Component {
-  state = {
-    users: [
-      {
-        id: "id1",
-        login: "mojo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        id: "id2",
-        login: "kino",
-        avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        id: "i3d",
-        login: "zoko",
-        avatar_url: "https://avatars0.githubusercontent.com/u/4?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-    ],
-  };
-
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
+        {users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 
 const userStyle = {
   display: "grid",
